36eed83e0a44350eda9791178516965367e5187d
Serhii Korniiuk <skorniiuk@peoplenetonline.com>
2019-08-15 15:02:14 +0300

    lol

--- paccar_support_oem_protractor/pages/edit.tool.page.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Cottomoeller on 12/9/2015.
- */
-var EditToolPage = function() {
-    this.entityTypeField = element.all(by.model('ctrl.chosenEntityType'));
-};
-
-module.exports = new EditToolPage();
--- paccar_support_oem_protractor/pages/export.page.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Cottomoeller on 12/30/2015.
- */
-var ExportPage = function() {
-    this.exportData = element.all(by.model('ctrl.chosenEntity'));
-};
-
-module.exports = new ExportPage();
